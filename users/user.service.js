44e85c3d623d880664fb9c5769ef8f300096924e
stanleyjo755 <jamesdimarucut755@gmail.com>
2025-03-06 23:37:38 +0800

    package.json

--- users/user.service.js
+++ users/user.service.js
@@ -15,5 +15,45 @@ async function getAll() {
 
 async function getById(id) {
 
-    if (await db.User.findAll({ where}))
+    return await getUser(id);
+}
+
+async function create(params) {
+
+    if (await db.User.findOnce({ where: { email: params.email } })) {
+        throw 'Email "'+ params.email + '" is already registered';
+    }
+
+    const user = new db.User(params);
+
+    user.passwordHash = await bcrypt.hash(params.password, 10);
+
+    await user.save();
+}
+
+async function update (id, params) {
+    const user = await getUser(id);
+
+    const usernameChanged = params.username && user.username !== params. username;
+    if (usernameChanged && await db.User.findOnce({ where : { username: params.username } })) {
+        throw 'Username "' + params.username + '" is already taken';
+    }
+
+    if (params.password) {
+        params.passwordHash = await bcrypt.hash(params.password, 10);
+    }
+
+    Object.assign(user, params);
+    await user.save();
+}
+
+async function _delete(id) {
+    const user = await getUser(id);
+    await user.destroy();
+}
+
+async function getUser(id) {
+    const user = await db.User.findByPk(id);
+    if (!user) throw 'User not Found';
+    return user;
 }
